b3600bb01ba5c04b9c20efff55e9ed7d569b7791
RobTrb <Robert.trb@gmail.com>
2025-04-11 18:11:47 +0200

    worked on the components beeing rendered in Game.jsx. Styling on the components is complete and the current functionality is complete so they are now prepaired for me to add further functionality and more components

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import "../styles/Game.css";
-import QuitGame from "./QuitGame"
-import Logo from "./Logo"
-import Rules from "./Rules"
-import GuessInput from "./GuessInput"
+import QuitGame from "./QuitGame";
+import Logo from "./Logo";
+import Rules from "./Rules";
+import GuessInput from "./GuessInput";
 
 function Game({ nrValue, letterRepeatValue, onBackToMenu }) {
-    
+  const [rulesModal, setRulesModal] = useState("rulesModalContainer");
+
   return (
     <div className="gameContainer">
-      <>      
-      <Logo />
-      <Rules nrValue={nrValue} letterRepeatValue={letterRepeatValue} />
-      <GuessInput />
-      <QuitGame onBackToMenu={onBackToMenu}/>
-      </>            
+      <>
+        <Logo />
+        <Rules
+          nrValue={nrValue}
+          letterRepeatValue={letterRepeatValue}
+          rulesModal={rulesModal}
+          setRulesModal={setRulesModal}
+        />
+        <GuessInput rulesModal={rulesModal} setRulesModal={setRulesModal} />
+        <QuitGame onBackToMenu={onBackToMenu} />
+      </>
     </div>
   );
 }
