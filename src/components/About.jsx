ab3d2478a1702cf9f6e373dea1eb7c2b6c232378
RobTrb <Robert.trb@gmail.com>
2025-04-18 20:28:48 +0200

    added description of the project on the about page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,9 @@ export default function About({ onBackToMenu }) {
   return (
     <div className="aboutContainer">
       <article className="aboutArticle">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed ad dolorum
-        iusto, suscipit odio minus perspiciatis dolores pariatur ex error eius
-        sit corrupti fugit officia cum eligendi tenetur deleniti maxime, enim
-        est velit et, quibusdam non nihil. Quibusdam nisi iusto doloribus saepe
-        praesentium obcaecati nemo, atque minus dicta enim distinctio.
+        This project was coded in React and built using Vite. The server is making use of Express and the highscores are stored and fetched from a database running in MongoDB 
+        with the use of Mongoose on the server side. The highscore list itself is server rendered with the use of Handlebars for express. Most of the game is run on the front end
+        except for some of the game logic that is run on the server to prevent cheating.
       </article>
       <button onClick={onBackToMenu}>Back to main menu</button>
     </div>
